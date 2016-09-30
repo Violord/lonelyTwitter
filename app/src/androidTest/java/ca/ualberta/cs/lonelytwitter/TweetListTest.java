4b1ae2fdd56ad6adab36d4b9a7438b18afb30f87
Violord <haroldisplaying@gmail.com>
2016-09-29 19:52:31 -0600

    Lab 4 Assignment

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -2,6 +2,8 @@ package ca.ualberta.cs.lonelytwitter;
 
 import android.test.ActivityInstrumentationTestCase2;
 
+import java.util.ArrayList;
+
 /**
  * Created by makepeac on 9/29/16.
  */
@@ -14,14 +16,14 @@ public class TweetListTest extends ActivityInstrumentationTestCase2 {
     public void testAddTweet(){
         TweetList tweets = new TweetList();
         Tweet tweet = new NormalTweet("adding tweet");
-        tweets.add(tweet);
+        tweets.addTweet(tweet);
         assertTrue(tweets.hasTweet(tweet));
     }
 
     public void testDelete(){
         TweetList list = new TweetList();
         Tweet tweet = new NormalTweet("test");
-        list.add(tweet);
+        list.addTweet(tweet);
         list.delete(tweet);
         assertFalse(list.hasTweet(tweet));
     }
@@ -29,15 +31,36 @@ public class TweetListTest extends ActivityInstrumentationTestCase2 {
     public void testGetTweet(){
         TweetList tweets = new TweetList();
         Tweet tweet = new NormalTweet("test");
-        tweets.add(tweet);
+        tweets.addTweet(tweet);
         Tweet returnedTweet = tweets.getTweet(0);
         assertEquals(returnedTweet.getMessage(), tweet.getMessage());
+    }
+
+    public void testGetTweets(){
+        TweetList tweets = new TweetList();
+        Tweet tweet = new NormalTweet("test");
+        tweets.addTweet(tweet);
+        ArrayList tweets2 = tweets.getTweets();
+        for(int i=0;i<=tweets.getCount();i++){
+            assertEquals(tweets.getTweet(i).getMessage(), tweets2.get(i));
+        }
+
     }
 
     public void testHasTweet(){
         TweetList list = new TweetList();
         Tweet tweet = new NormalTweet("test");
-        list.add(tweet);
+        list.addTweet(tweet);
         assertTrue(list.hasTweet(tweet));
+    }
+
+    public void testGetCount(){
+        TweetList list = new TweetList();
+        Tweet tweet = new NormalTweet("test");
+        list.addTweet(tweet);
+        int returnedCount = list.getCount();
+        list.addTweet(tweet);
+        int returnedCount2 = list.getCount();
+        assertEquals(returnedCount, returnedCount2);
     }
 }
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
@@ -16,15 +16,37 @@ public class TweetList {
         return tweets.get(index);
     }
 
+
     public boolean hasTweet(Tweet tweet){
         return tweets.contains(tweet);
     }
 
-    public void add(Tweet tweet) {
+    public void addTweet(Tweet tweet) {
+        if(tweets.contains(tweet)){
+            throw new IllegalArgumentException();
+        }else{
         tweets.add(tweet);
+        }
     }
 
     public void delete(Tweet tweet) {
         tweets.remove(tweet);
     }
+
+
+    public int getCount(){return tweets.size();}
+
+    public ArrayList getTweets() {
+        for(int i=0;i<tweets.size();i++){
+            for(int j=i;j<tweets.size();j++){
+                if(tweets.get(j).getDate().compareTo(tweets.get(j+1).getDate()) == 1){
+                    Tweet temp = tweets.get(j);
+                    tweets.set(j, tweets.get(j+1));
+                    tweets.set(j+1, temp);
+                }
+            }
+        }
+        return tweets;
+    }
+
 }
